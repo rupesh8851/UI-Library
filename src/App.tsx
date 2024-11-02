c19dad9844300711ba94ff8f010baa26adddace7
rupesh8851 <rupeshsharmayo@gmail.com>
2024-11-02 16:09:52 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar.tsx';
+import { Home } from './pages/Home.tsx';
 
 function App() {
-  return <div className="text-2xl text-purple-500">My Application</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
