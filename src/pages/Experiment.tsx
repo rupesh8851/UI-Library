695e07b2ac6bff7d48e3653793e8ab9215993283
rupesh8851 <rupeshsharmayo@gmail.com>
2024-12-30 02:01:34 +0530

    removed experiment

--- src/pages/Experiment.tsx
+++ src/pages/Experiment.tsx
@@ -1,24 +1,3 @@
-import { Divider } from '../components/common/Divider.tsx';
-import { FormControl } from '../components/common/FormControl.tsx';
-import { FormErrorTextHelper } from '../components/common/FormErrorTextHelper.tsx';
-import { FormInput } from '../components/common/FormInput.tsx';
-import { FormLabel } from '../components/common/FormLabel.tsx';
-import { Stepper } from '../components/common/Stepper.tsx';
-
 export const Experiment = () => {
-  return (
-    <div className="flex flex-col justify-center items-center max-w-lg  py-8 mx-auto mt-16 bg-white shadow-xl rounded-lg">
-      <Stepper
-        steps={['Personal details', 'Account Details', 'Strategy']}
-        currentStep={0}
-        width="150px"
-      />
-      <Divider />
-      <FormControl isInvalid={false}>
-        <FormLabel label="Name" />
-        <FormInput value="" onChange={() => {}} placeholder="Enter the Name" />
-        <FormErrorTextHelper message="Name is required" />
-      </FormControl>
-    </div>
-  );
+  return <div>Experiment</div>;
 };
